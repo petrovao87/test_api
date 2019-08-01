d1c5cc539c19531caef95c982f82a01cd9e562c1
petrovao87 <petrovao87@mail.ru>
2019-08-01 21:06:11 +0300

    "update api func and db"

--- api.py
+++ api.py
@@ -1,27 +1,16 @@
-import flask
-from flask import request, jsonify
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+from requests import put, get
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
+app = Flask(__name__)
+api = Api(app)
+
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
 
 
 @app.route('/', methods=['GET'])
@@ -30,32 +19,120 @@ def home():
 <p>A prototype API for distant reading of science fiction novels.</p>'''
 
 
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
 
 
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
+parser = reqparse.RequestParser()
+parser.add_argument('task')
 
-    # Create an empty list for our results
-    results = []
 
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
+# Todo
+# shows a single todo item and lets you delete a todo item
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
 
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    return jsonify(results)
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
 
-app.run()
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# put('http://localhost:5000/todo1', data={'data': 'Remember the milk'}).json()
+#
+# get('http://localhost:5000/todo1').json()
+
+
+# import flask
+# from flask import request, jsonify
+#
+# app = flask.Flask(__name__)
+# app.config["DEBUG"] = True
+#
+# # Create some test data for our catalog in the form of a list of dictionaries.
+# books = [
+#     {'id': 0,
+#      'title': 'A Fire Upon the Deep',
+#      'author': 'Vernor Vinge',
+#      'first_sentence': 'The coldsleep itself was dreamless.',
+#      'year_published': '1992'},
+#     {'id': 1,
+#      'title': 'The Ones Who Walk Away From Omelas',
+#      'author': 'Ursula K. Le Guin',
+#      'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+#      'published': '1973'},
+#     {'id': 2,
+#      'title': 'Dhalgren',
+#      'author': 'Samuel R. Delany',
+#      'first_sentence': 'to wound the autumnal city.',
+#      'published': '1975'}
+# ]
+#
+#
+# @app.route('/', methods=['GET'])
+# def home():
+#     return '''<h1>Distant Reading Archive</h1>
+# <p>A prototype API for distant reading of science fiction novels.</p>'''
+#
+#
+# @app.route('/api/v1/resources/books/all', methods=['GET'])
+# def api_all():
+#     return jsonify(books)
+#
+#
+# @app.route('/api/v1/resources/books', methods=['GET'])
+# def api_id():
+#     # Check if an ID was provided as part of the URL.
+#     # If ID is provided, assign it to a variable.
+#     # If no ID is provided, display an error in the browser.
+#     if 'id' in request.args:
+#         id = int(request.args['id'])
+#     else:
+#         return "Error: No id field provided. Please specify an id."
+#
+#     # Create an empty list for our results
+#     results = []
+#
+#     # Loop through the data and match results that fit the requested ID.
+#     # IDs are unique, but other fields might return many results
+#     for book in books:
+#         if book['id'] == id:
+#             results.append(book)
+#
+#     # Use the jsonify function from Flask to convert our list of
+#     # Python dictionaries to the JSON format.
+#     return jsonify(results)
+#
+# app.run()
--- db_api_test.py
+++ db_api_test.py
@@ -1,19 +1,41 @@
 from db import db_session, Process, ProcessPerformer #ProcessParameter, ProcessQuota, ProcessStartCondition
-
-
-# in_db = db_session.query(Process).filter(CoinBase.coin_name == name_coin.lower()).first()
-#         if not coin_in_db:
-perf = ProcessPerformer(1, 'name', 'description')
-coin = Process(1, 'name', 'description', 'flag', 100)
-db_session.add(perf)
-db_session.add(coin)
-db_session.commit()
+#
+#
+# # in_db = db_session.query(Process).filter(CoinBase.coin_name == name_coin.lower()).first()
+# #         if not coin_in_db:
+#
+#
+#
+# perf = ProcessPerformer(process_performer_id_1=1, name='name', description='description')
+# coin = Process(process_id=1, process_name='name', description='description', activity_flag='flag',
+#                process_performer_id=1)
+#
+# perf2 = ProcessPerformer(process_performer_id_1=2, name='name2', description='description2')
+# coin2 = Process(process_id=2, process_name='name2', description='description2', activity_flag='flag2',
+#                process_performer_id=1)
+#
+# coin3 = Process(process_id=3, process_name='name3', description='description3', activity_flag='flag3',
+#                 process_performer_id=2)
+#
+#
+# db_session.add(perf)
+# db_session.add(coin)
+# db_session.add(perf2)
+# db_session.add(coin2)
+# db_session.add(coin3)
+#
+# db_session.commit()
 print('update base')
 
 user_query = db_session.query(Process).join(ProcessPerformer).\
-        filter(Process.process_performer_id == ProcessPerformer.id).first()
+    filter(Process.process_performer_id == 1).all()
 print(user_query)
-# user_query.process_performer_id = 200
+for i in range(len(user_query)):
+    user_query[i].process_performer_id = 2
+    print(user_query[i].process_performer_id)
+    db_session.commit()
+
+print('end')
 
 
 # activity_flag = Column(String(50))
